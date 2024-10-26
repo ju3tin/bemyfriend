ca3890e21163257c5732666a7ffc7522d89be626
ju3tin <ju3tin@hotmail.co.uk>
2024-10-26 03:41:47 +0100

    wer

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,25 @@
 import { allPosts } from "@/.contentlayer/generated"
 import Link from "next/link"
+//import { useSession, signIn, signOut } from 'next-auth/react';
+
 
 export default function Home() {
+  //const { data, status } = useSession();
+ // if (status === 'loading') return <h1> loading... please wait</h1>;
+  {
+    return (
+      <div>
+        <h1> hi <script></script></h1>
+        <img src='SAD' alt="ADS" />
+        <button className="ASD">sign out</button>
+      </div>
+    );
+  }
   return (
     <div className="prose dark:prose-invert">
      Yo
+     <button onClick={() => ('google')}>sign in with gooogle</button>
+   
     </div>
   )
 }
