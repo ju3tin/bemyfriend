4c4a42bff5fefc9ac42689354632e0d5593cc92d
ju3tin <ju3tin@hotmail.co.uk>
2024-10-26 03:59:04 +0100

    wer

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,3 @@
-export default function Page() {
+export default function Home() {
     return <p>Dashboard Page</p>;
   }
--- app/page.tsx
+++ app/page.tsx
@@ -9,17 +9,24 @@ export default function Home() {
   {
     return (
       <div>
+         <script src="https://accounts.google.com/gsi/client" async></script>
+    <div id="g_id_onload"
+        data-client_id="YOUR_GOOGLE_CLIENT_ID"
+        data-login_uri="https://your.domain/your_login_endpoint"
+        data-auto_prompt="false">
+    </div>
+    <div className="g_id_signin"
+        data-type="standard"
+        data-size="large"
+        data-theme="outline"
+        data-text="sign_in_with"
+        data-shape="rectangular"
+        data-logo_alignment="left">
+    </div>
         <h1> hi <script></script></h1>
-        <img src='SAD' alt="ADS" />
         <button className="ASD">sign out</button>
       </div>
     );
   }
-  return (
-    <div className="prose dark:prose-invert">
-     Yo
-     <button onClick={() => ('google')}>sign in with gooogle</button>
-   
-    </div>
-  )
+ 
 }
